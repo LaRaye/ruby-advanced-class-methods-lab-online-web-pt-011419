46da8330a75cc42fc2632d800374ba2e058089b0
LaRaye Johnson <lnj2105@gmail.com>
2019-02-09 21:44:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,43 @@ class Song
     self.class.all << self
   end
 
+  def self.create 
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def Song.new_by_name(name)
+    song = self.new
+    song.name = name
+    song 
+  end
+
+  def Song.create_by_name(name)
+    song = self.new
+    song.name = name 
+    @@all << song
+    song
+  end
+
+  def Song.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def Song.find_or_create_by_name(name)
+    song = Song.find_by_name(name)
+    
+    if song == nil
+      Song.create_by_name(name)
+    elsif song.name == name
+      song
+    end
+  end
+  
+  def Song.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+  
+  def Song.new_from_filename
+   
 end
